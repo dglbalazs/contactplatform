ff1604d681fa01dbec0a0a22c1dffaedbb5e6dc4
diagolbalazs <diagolbalazs@gmail.com>
2023-10-11 05:25:45 +0200

    Updated Header - Desktop UI ready

--- app/components/Contacts/header/ContactsHeader.tsx
+++ app/components/Contacts/header/ContactsHeader.tsx
@@ -2,6 +2,8 @@
 
 import styles from "./ContactsHeader.module.scss"
 import { useState } from 'react';
+import Text from "../../Utility/Text";
+import Button from "../../Utility/Button";
 
 interface HeaderProps {
     theme: string,
@@ -17,9 +19,40 @@ const ContactsHeader: React.FC<Readonly<HeaderProps>> = ({ theme, setTheme}) => 
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.backbtn}>Back</div>
-            <div className={styles.mainhead}>Header</div>
-            <div className={styles.lightdark} onClick={toggleTheme}>Light dark mode</div>
+            <div className={styles.backbtn}>
+                <Button
+                        buttonType="onlyicon"
+                        colorType="deep"
+                        icon="Back"
+                        text=""
+                    />
+            </div>
+            <div className={styles.mainhead}>
+                <Text text="Contacts" texttype={1} clr={1}></Text>
+                <div className={styles.submenu}>
+                    <Button
+                        buttonType="onlyicon"
+                        colorType="deep"
+                        icon="Settings"
+                        text=""
+                    />
+                    <img src="/icons/Photo.svg"></img>
+                    <Button
+                        buttonType="both"
+                        colorType="grad"
+                        icon="Add"
+                        text="Add Item"
+                    />
+                </div>
+            </div>
+            <div className={styles.lightdark} onClick={toggleTheme}>
+            <Button
+                        buttonType="onlyicon"
+                        colorType="deep"
+                        icon="Light"
+                        text=""
+                    />
+            </div>
         </div>
     )
 }
