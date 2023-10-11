2c15c64e73ebd379c0cc4446e595587fff29bdc0
diagolbalazs <diagolbalazs@gmail.com>
2023-10-11 05:25:02 +0200

    Added Icon and Button Utility Component

---
+++ app/components/Utility/Button.tsx
@@ -0,0 +1,43 @@
+import styles from './Button.module.scss'
+import Icon from './Icon';
+import Text from './Text';
+
+
+interface ButtonProps {
+    buttonType: 'both' | 'onlytext' | 'onlyicon';
+    colorType: 'normal' | 'deep' | 'grad';
+    icon: 'Add' | 'Back' | 'Call' | 'Change' | 'Delete' | 'Fav' | 'Light' | 'More' | 'Mute' | 'Search' | 'Settings';
+    text: string;
+  }
+
+const Button: React.FC<ButtonProps> = ({ buttonType, colorType, icon, text }) => {
+
+    const buttonClassNames = [
+        styles.button,
+        styles[buttonType],
+        styles[colorType],
+      ].join(' ');
+    
+      const iconClassNames = [
+        styles.icon,
+        styles[`icon-${icon}`],
+      ].join(' ');
+
+
+return (
+    <button className={buttonClassNames}>
+        {buttonType === 'both' && (
+        <>
+            <Icon iconname={icon}></Icon>
+            <Text text={text} texttype={4} clr={1}></Text>
+        </>
+        )}
+        {buttonType === 'onlyicon' && (
+            <Icon iconname={icon}></Icon>
+        )}
+        {buttonType === 'onlytext' && <Text text={text} texttype={4} clr={1}></Text>}
+    </button>
+  );
+}
+
+export default Button;
