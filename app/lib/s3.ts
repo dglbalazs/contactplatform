4d1e1231498b49eda213436d86919c4329f4db5a
diagolbalazs <diagolbalazs@gmail.com>
2023-10-12 13:57:12 +0200

    updated s3

--- app/lib/s3.ts
+++ app/lib/s3.ts
@@ -23,30 +23,10 @@ export async function generateUploadURL(contactId: number) {
     const params = ({
       Bucket: bucketName,
       Key: contactId.toString(),
-      Expires: 60
     })
     
     const uploadURL = await s3.getSignedUrlPromise('putObject', params)
     return uploadURL
   }
-
-
-export async function uploadImageToBucket(file : File, contactId: number) {
-    
-    const params = ({
-        Body: file,
-        Bucket: bucketName,
-        Key: contactId.toString(),
-        ContentType: file.type, // Set the content type from the file
-    })
-
-    try {
-        const result = await s3.putObject(params).promise();
-        console.log("Sikeresen feltöltve - ", result);
-    } catch (error) {
-        console.error("Hiba a feltöltés során - ", error);
-        throw error;
-    }
-}
 
 export const s3BaseUrl = "https://" + bucketName + ".s3.eu-central-1.amazonaws.com/"
